6d90d28c442bee7bed71ed84d15b0110b6b2c79b
cbipok <alex_andr_6@mail.ru>
2021-10-01 19:12:28 +0700

    ver1

--- lect3/lab3.py
+++ lect3/lab3.py
@@ -2,11 +2,61 @@ import mymatrices
 
 
 class Accountant:
-    def __init__(self):
-        self._workers_salaries = {}
+    def __init__(self, name="Бухгалтер"):
+        self._workers_transaction = {}
+        self._name = name
+
+    def record_worker(self, worker):
+        if isinstance(worker, Datascientist):
+            workers_list = self._workers_transaction.keys()
+            if not (worker in workers_list):
+                self._workers_transaction.update({worker: 0})
+                print(f"{self._name} учёл работника {worker.name}")
+                return True
+            print(f"{worker.name} уже учтён")
+            return True
+        return False
 
     def give_salary(self, worker, amount):
-        worker(self, amount)
+        if isinstance(worker, Datascientist):
+            workers_list = self._workers_transaction.keys()
+            if worker in workers_list:
+                self._workers_transaction.update({worker: amount})
+                worker.take_salary(amount, self)
+                print(f"{self._name} подтверждает, что {worker.name} получил ${amount}")
+                return True
+        print(f"{self._name} не знает {worker}")
+        return False
+
+    def __call__(self, worker, amount):
+        if isinstance(worker, Datascientist):
+            workers_list = self._workers_transaction.keys()
+            if worker in workers_list:
+                transaction = self._workers_transaction.get(worker)
+                if transaction == amount:
+                    self._workers_transaction.update({worker: 0})
+                    print(f"{self._name} одобряет перечисление работнику {worker.name} ${amount}")
+                    return True
+                else:
+                    print(f"{self._name} запрещает перечисление работнику {worker.name} ${amount}")
+                    return False
+            print(f"{self._name} не знает {worker}")
+            return False
+        print(f"{self._name} не знает {worker}")
+        return False
+
+    ##def __check_transaction(self):
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def workers(self):
+        workers = list(self._workers_transaction.keys())
+        for i in range(len(workers)):
+            workers[i] = workers[i].name
+        return workers
 
 
 class Datascientist:
@@ -21,12 +71,12 @@ class Datascientist:
     def do_work(self, filename1, filename2):
         if self.__work == "что-то делать":
             print("Я не знаю, что мне делать")
-            return None
+            return False
         matrix1 = mymatrices.read_matrix(filename1)
         matrix2 = mymatrices.read_matrix(filename2)
         if not mymatrices.check_matrices(matrix1, matrix2):
             print(f"Не могу {(self.__dict.get(self.__work))[0]} матрицы")
-            return None
+            return False
         matrix_res = []
         for i in range(len(matrix1)):
             row = []
@@ -34,14 +84,26 @@ class Datascientist:
                 row.append((self.__dict.get(self.__work))[1](matrix1[i][j], matrix2[i][j]))
             matrix_res.append(row)
         mymatrices.matrix_out(matrix_res)
+        return True
 
-    def __take_salary(self, x):
-        self.__money += x
-
-    def __call__(self, acc, amount):
+    def take_salary(self, acc, amount):
         if isinstance(acc, Accountant):
-            self.__take_salary(amount)
+            if acc(self, amount):
+                self.__money += amount
+                print(f"{self.__name} получил ${amount}")
+                return True
+            print(f"{self.__name} не получил ${amount}")
+            return False
+        print(f"Нет такого бухгалтера {acc}")
+        return False
 
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def wallet(self):
+        return self.__money
 
 
 class Pupa(Datascientist):
@@ -56,11 +118,3 @@ class Lupa(Datascientist):
         work = "вычетать матрицы"
         super().__init__(name, money, work)
         print(f"Датасайнтист {name} готов {work}")
-
-
-worker1 = Pupa("Алёша")
-worker1.do_work("matrices\\1.txt", "matrices\\2.txt")
-nachalnika = Accountant(worker1)
-nachalnika.give_salary(worker1, 300)
-worker2 = Lupa()
-worker2.do_work("matrices\\1.txt", "matrices\\2.txt")
