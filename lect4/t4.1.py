f58ffb41aecd7cc975ebfec3a3b655f1d45d377c
cbipok <alex_andr_6@mail.ru>
2021-10-02 15:13:05 +0700

    meh

---
+++ lect4/t4.1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+# def function(x):
+
+
+arr_size = 90
+arr = np.random.randint(10, size=arr_size)
+print(f"Входной массив: {arr}\n")
+freqs = np.unique(arr, return_counts=True)
+arr_to_sort = np.array((freqs[0], freqs[1]), dtype=tuple)
+arr_to_sort = np.transpose(arr_to_sort, axes=None)
+final = arr_to_sort[np.argsort(arr_to_sort[:, 1])]
+final = np.transpose(final, axes=None)
+print(f'Элементы массива, sorted по частоте: {list(final[0])[::-1]}\n')
